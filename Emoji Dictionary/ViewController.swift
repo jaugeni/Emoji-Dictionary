1b6d9d6f1b2cb3096438015a46af79636c433d28
jaugeni <jaugeni@gmail.com>
2016-11-01 19:21:53 -0400

    Add emojis and vork

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
 
     @IBOutlet weak var tblViewController: UITableView!
     
+    var emojis = ["😀","😋","🤑","😈","🐕","🌧","🎫","💲"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,12 +23,12 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emojis.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "😋"
+        cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
     
